9cdc97e482df55c0f0a3ee90919bbaa7dc376ae2
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-02-03 13:05:08 +1100

    commit tree c0e11591566c17e7b7a4fdcbeb472939e382e2e0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title (title)
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.all.first
+    end
+
+    def self.last_movie
+       self.all.last 
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.all.find(id == id)
+    end
+    
+    def self.find_movie_with_attributes attributes
+        self.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        self.where('release_date > 2002')
+    end
+
+    def update_with_attributes (attributes)
+        self.update(attributes)
+    end
+
+    def self.update_all_titles (attributes)
+        self.update_all(title: attributes)
+    end
+
+    def self.delete_by_id (id)
+        self.delete(id == id)
+    end
+
+    def self.delete_all_movies
+        self.delete_all
+    end
 end
